04aa96f02af9ec67cee4381ba7902f3fdd5df28a
JF25628 <a25628@alunos.ipca.pt>
2024-12-02 12:06:26 +0000

    Project Structure

---
+++ FireBaseProject/app/src/main/java/com/example/firebaseproject/MainActivity.kt
@@ -0,0 +1,170 @@
+package com.example.firebaseproject
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.firebaseproject.ui.theme.FireBaseProjectTheme
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
+
+class MainActivity : ComponentActivity() {
+    lateinit var auth: FirebaseAuth
+    val db = Firebase.firestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            auth = Firebase.auth
+
+            FireBaseProjectTheme {
+
+            }
+        }
+    }
+
+    fun registerUser(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    val show = Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+
+    fun loginUser(email: String, password: String)
+    {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+
+    fun createUser() {
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815,
+        )
+
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+    }
+
+//    val user = Firebase.auth.currentUser
+//    user?.let {
+//        // Name, email address, and profile photo Url
+//        val name = it.displayName
+//        val email = it.email
+//        val photoUrl = it.photoUrl
+//
+//        // Check if user's email is verified
+//        val emailVerified = it.isEmailVerified
+//
+//        // The user's ID, unique to the Firebase project. Do NOT use this value to
+//        // authenticate with your backend server, if you have one. Use
+//        // FirebaseUser.getIdToken() instead.
+//        val uid = it.uid
+//    }
+
+
+
+
+
+
+
+//// Add a new document with a generated ID
+//    db.collection("users")
+//    .add(user)
+//    .addOnSuccessListener { documentReference ->
+//        Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+//    }
+//    .addOnFailureListener { e ->
+//        Log.w(TAG, "Error adding document", e)
+//    }
+//
+//
+//    db.collection("users")
+//    .get()
+//    .addOnSuccessListener { result ->
+//        for (document in result) {
+//            Log.d(TAG, "${document.id} => ${document.data}")
+//        }
+//    }
+//    .addOnFailureListener { exception ->
+//        Log.w(TAG, "Error getting documents.", exception)
+//    }
+
+
+
+
+
+    /*
+    *
+    * Tema: Loja com contas e carrinho de compras
+    * é possivel partilhar o carrinho de compras com outros utilizadores
+    *
+    *
+    * */
+
+
+    /*
+    *
+    * Login
+    * Lista  de produtos
+    * Adicionar lista
+    * botao para o carrinho
+    * comprar
+    * partilhar carrinho
+    *
+    * */
+
+
+
+
+}
+
