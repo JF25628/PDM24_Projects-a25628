06f3d6ee6b508e9c1cc716f1cb58de5fea026cc5
JF25628 <a25628@alunos.ipca.pt>
2025-01-08 18:51:51 +0000

    clean

--- FireBaseProject/app/src/main/java/com/example/firebaseproject/app/data/remote/api/AuthApi.kt
+++
@@ -1,9 +0,0 @@
-package com.example.firebaseproject.app.data.remote.api
-
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
-
-interface AuthApi {
-    fun loginWithEmailAndPassword(email: String, password: String): Task<AuthResult>
-    fun createUserWithEmailAndPassword(email: String, password: String): Task<AuthResult>
-}
--- FireBaseProject/app/src/main/java/com/example/firebaseproject/app/domain/repository/ProductRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.firebaseproject.app.domain.repository
-
-import com.example.firebaseproject.app.domain.model.ProductModel
-
-interface ProductRepository {
-    suspend fun getAllProducts(): List<ProductModel>
-    suspend fun getCartItems(userId: String): Int
-}
